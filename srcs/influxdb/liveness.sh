2ae76ccc9b150a9a26c7db0493ea22a801b7457a
tontonpizzaé <ng.vosong@gmail.com>
2020-12-12 14:35:46 +0100

    -liveness

--- srcs/influxdb/liveness.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ $(ps -ef | grep "influxd" | wc -l) -ge 2 ] \
-	&& [ $(ps -ef | grep "/telegraf/telegraf" | wc -l) -ge 2 ]; then
-	echo "All processes are working"
-else
-	echo "There is a problem" && exit 1
-fi
